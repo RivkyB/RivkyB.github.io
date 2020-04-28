5bf2f94a44d66312978b102ac6d0c726a0cacab9
Rivky Bleier <rivky@ezrirx.com>
2020-04-27 23:25:54 -0400

    Updates

---
+++ precache-manifest.22f2ae7f9a518ab18781ef64b87d7b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0732cc0df11256b93081b080c8ecb3ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039bd21c96f15d1e8031",
+    "url": "/static/css/main.904d8837.chunk.css"
+  },
+  {
+    "revision": "42b4bac9b64f683ee5ca",
+    "url": "/static/js/2.45863630.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/static/js/2.45863630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039bd21c96f15d1e8031",
+    "url": "/static/js/main.019299ce.chunk.js"
+  },
+  {
+    "revision": "eb2da8a6912a8c64fb47",
+    "url": "/static/js/runtime-main.555d898a.js"
+  }
+]);
